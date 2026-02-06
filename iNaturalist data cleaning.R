d05f2becde8762f19126b56f1eba166e43ec2d30
jianyoulee <78127355+jianyoulee@users.noreply.github.com>
2026-02-06 10:02:11 +0100

    Update iNaturalist data cleaning.R

--- iNaturalist data cleaning.R
+++ iNaturalist data cleaning.R
@@ -39,40 +39,40 @@ clean_inat <- function(csv_file){
 }
 
 #applying to the data 
-inat2023 <- clean_inat('D:/R/inaturalist/observations-2023.csv')
+inat2023 <- clean_inat('./observations-2023.csv')
 count(inat2023$iconic_taxon_name)
 inat2023[1,date_time]
-inat2022 <- clean_inat('D:/R/inaturalist/observations-2022.csv')
+inat2022 <- clean_inat('./observations-2022.csv')
 count(inat2022$iconic_taxon_name)
 inat2022[1,date_time]
-inat2021 <- clean_inat('D:/R/inaturalist/observations-2021.csv')
+inat2021 <- clean_inat('./observations-2021.csv')
 count(inat2021$iconic_taxon_name)
 inat2021[1,date_time]
-inat2020 <- clean_inat('D:/R/inaturalist/observations-2020.csv')
+inat2020 <- clean_inat('./observations-2020.csv')
 count(inat2020$iconic_taxon_name)
 inat2020[1,date_time]
-inat2019 <- clean_inat('D:/R/inaturalist/observations-2019.csv')
+inat2019 <- clean_inat('./observations-2019.csv')
 count(inat2019$iconic_taxon_name)
 inat2019[1,date_time]
-inat2018 <- clean_inat('D:/R/inaturalist/observations-2018.csv')
+inat2018 <- clean_inat('./observations-2018.csv')
 count(inat2018$iconic_taxon_name)
 inat2018[1,date_time]
-inat2017 <- clean_inat('D:/R/inaturalist/observations-2017.csv')
+inat2017 <- clean_inat('./observations-2017.csv')
 count(inat2017$iconic_taxon_name)
 inat2017[1,date_time]
 
 #save
-write.csv(inat2022, file = 'D:/R/inaturalist/cleaned_data/inat2022.csv', row.names = FALSE)
-write.csv(inat2021, file = 'D:/R/inaturalist/cleaned_data/inat2021.csv', row.names = FALSE)
-write.csv(inat2020, file = 'D:/R/inaturalist/cleaned_data/inat2020.csv', row.names = FALSE)
-write.csv(inat2019, file = 'D:/R/inaturalist/cleaned_data/inat2019.csv', row.names = FALSE)
-write.csv(inat2018, file = 'D:/R/inaturalist/cleaned_data/inat2018.csv', row.names = FALSE)
-write.csv(inat2017, file = 'D:/R/inaturalist/cleaned_data/inat2017.csv', row.names = FALSE)
+write.csv(inat2022, file = './cleaned_data/inat2022.csv', row.names = FALSE)
+write.csv(inat2021, file = './cleaned_data/inat2021.csv', row.names = FALSE)
+write.csv(inat2020, file = './cleaned_data/inat2020.csv', row.names = FALSE)
+write.csv(inat2019, file = './cleaned_data/inat2019.csv', row.names = FALSE)
+write.csv(inat2018, file = './cleaned_data/inat2018.csv', row.names = FALSE)
+write.csv(inat2017, file = './cleaned_data/inat2017.csv', row.names = FALSE)
 
 #combine all in one, and to reduce file size, further select less columns
 inat_malaysia <- rbind(inat2017, inat2018, inat2019, inat2020, inat2021, inat2022, inat2023)
 inat_malaysia <- select(inat_malaysia, 'id', 'date_time', 'date', 'time', 'common_name','iconic_taxon_name','taxon_kingdom_name','scientific_name','latitude','longitude')
-write.csv(inat_malaysia, file = 'D:/R/inaturalist/cleaned_data/inat_malaysia.csv', row.names = FALSE)
+write.csv(inat_malaysia, file = './cleaned_data/inat_malaysia.csv', row.names = FALSE)
 
 #city nature challenge years and dates 
 #2018 April 27-30
@@ -90,23 +90,23 @@ cnc2023 <- inat2023 %>% filter(between(date, as.Date('2023-04-28'),as.Date('2023
 
 cnc <- rbind(cnc2018, cnc2019, cnc2020, cnc2021, cnc2022, cnc2023)
 
-write.csv(cnc, file = 'D:/R/inaturalist/cleaned_data/cnc.csv', row.names = FALSE)
+write.csv(cnc, file = './cleaned_data/cnc.csv', row.names = FALSE)
 
-klang <- lapply(list.files(path = 'D:/R/inaturalist', pattern = '_id.csv', full.names = TRUE), fread)
+klang <- lapply(list.files(path = '.', pattern = '_id.csv', full.names = TRUE), fread)
 klang <- rbindlist(klang)
 klang$region <- 'kl-selangor'
 
-penang <- lapply(list.files(path = 'D:/R/inaturalist', pattern = '_idp.csv', full.names = TRUE), fread)
+penang <- lapply(list.files(path = '.', pattern = '_idp.csv', full.names = TRUE), fread)
 penang <- rbindlist(penang)
 penang$region <- 'penang'
 
 seremban <- fread('D:/R/inaturalist/cnc2021_ids.csv')
 seremban$region <- 'seremban'
 
-jb <- lapply(list.files(path = 'D:/R/inaturalist', pattern = '_idjb.csv', full.names = TRUE), fread)
+jb <- lapply(list.files(path = '.', pattern = '_idjb.csv', full.names = TRUE), fread)
 jb <- rbindlist(jb)
 jb$region <- 'johor-bahru'
 
 cnc_id <- rbind(klang, penang, seremban, jb)
 cnc_complete <- merge(x=cnc_id, y=cnc, by = 'id')
-write.csv(cnc_complete, file = 'D:/R/inaturalist/cleaned_data/cnc_complete.csv', row.names = FALSE)
+write.csv(cnc_complete, file = './cleaned_data/cnc_complete.csv', row.names = FALSE)
